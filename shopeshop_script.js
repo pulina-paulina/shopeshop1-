4508336052e1d3fe97d6826cf2c05658d8631fd7
den2736 <babencko.denis2011@yandex.ru>
2019-04-11 20:31:17 +0300

    Sliders

--- shopeshop_script.js
+++ shopeshop_script.js
@@ -1,32 +1,36 @@
 //слайдер в шапке (первый способ как на курсах)
-/*
-var slider=document.querySelector("slider");
-var slider_point=document.querySelector("slider_point");
+
+var slider = document.querySelector(".slider");
+var slider_points = slider.querySelectorAll(".slider_point");
 var sliderList = document.querySelector('.slider_list');
 
 
-slider_point.querySelectorAll("slider_point").forEach(function(elem, index){
-	elem.addEventListener('click', function(){
-		slider_points.querySelector('.current').classList.remove('current');
+slider_points.forEach(function (elem, index) {
+	elem.addEventListener('click', function () {
+		slider.querySelector('.current').classList.remove('current');
 		slider.querySelector('.slider_item_show').classList.remove('slider_item_show');
 
 		this.classList.add('current');
-		var slider = sliderList.children[index]; //не поняла 
-		slider.classList.add('slider_item_show');
+		var slideToShow = sliderList.children[index]; //не поняла 
+		slideToShow.classList.add('slider_item_show');
 	});
-});*/
+});
 
-//slider2 (спозов 2)
+//slider2 (способ 2)
 
 
-var slides = document.querySelectorAll('#slide2_list .slider2_item');
+var slider2 = document.querySelector('.slider2');
+var slides = slider2.querySelectorAll('#slide2_list .slider2_item');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
- 
+var slideInterval = setInterval(nextSlide, 2000);
+slider2.querySelector('.pagination_last').textContent = slides.length;
+
 function nextSlide() {
-    slides[currentSlide].className ='slider2_item';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slider2_item showing';
+	slides[currentSlide].classList.remove('showing');
+	currentSlide = (currentSlide + 1) % slides.length;
+	slides[currentSlide].classList.add('showing');
+
+	slider2.querySelector('.pagination_now').textContent = currentSlide + 1;
 }
 
 
